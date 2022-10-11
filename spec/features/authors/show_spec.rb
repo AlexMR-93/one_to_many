0b23eb2a08d3a4b88e904ba31a5f8479c4e37a79
Alex M <alexm0717@hotmail.com>
2022-10-11 16:08:51 -0700

    story two complete

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -1,17 +1,18 @@
 require "rails_helper"
 
 
-RSpec.describe("Authors index") do
+RSpec.describe("Authors show page") do
   before(:each) do
     @author1 = Author.create!(    name: " Alex",     has_won_awards: false,     books_written: 1)
     @author2 = Author.create!(    name: " Peyton",     has_won_awards: true,     books_written: 2)
     @author3 = Author.create!(    name: " Gabe",     has_won_awards: false,     books_written: 3)
   end
 
-  it(" #1.name of each author record in system") do
-    visit("/authors")
+  it("parent with that id including the parent's attributes") do
+    visit("/authors/#{@author1.id}")
+    save_and_open_page
     expect(page).to(have_content("Authors name:#{@author1.name}"))
-    expect(page).to(have_content("Authors name:#{@author2.name}"))
-    expect(page).to(have_content("Authors name:#{@author3.name}"))
+    expect(page).to(have_content("Author has has won awards:#{@author1.has_won_awards}"))
+    expect(page).to(have_content("Author books written:#{@author1.books_written}"))
   end
 end
