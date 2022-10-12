b549fc72d1dca5df28fb2d8de4b5c36644e42690
Alex M <alexm0717@hotmail.com>
2022-10-12 15:32:26 -0700

    story seven complete

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe("Authors show page") do
   before(:each) do
     @author1 = Author.create!(    name: " Alex",     has_won_awards: false,     books_written: 1)
     @book1 = @author1.books.create!(    title: "Spiderwick",     best_seller: true,     pages: 120)
+    @book4 = @author1.books.create!(    title: "Wickspider",     best_seller: false,     pages: 300)
     @author2 = Author.create!(    name: " Peyton",     has_won_awards: true,     books_written: 2)
     @book2 = @author2.books.create!(    title: "Harry Potter",     best_seller: true,     pages: 140)
     @author3 = Author.create!(    name: " Gabe",     has_won_awards: false,     books_written: 3)
@@ -13,9 +14,14 @@ RSpec.describe("Authors show page") do
 
   it("#2.parent with that id including the parent's attributes") do
     visit("/authors/#{@author1.id}")
-    save_and_open_page
     expect(page).to(have_content("Authors name:#{@author1.name}"))
     expect(page).to(have_content("Author has has won awards:#{@author1.has_won_awards}"))
     expect(page).to(have_content("Author books written:#{@author1.books_written}"))
+  end
+
+  it("#7.count of the number of children associated with this parent") do
+    visit("/authors/#{@author1.id}")
+    save_and_open_page
+    expect(page).to(have_content("Number of books associated with author:#{@author1.book_count}"))
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -16,12 +16,18 @@ RSpec.describe(Author, type: :model) do
   describe("model method") do
     before(:each) do
       @author1 = Author.create!(      name: " Alex",       has_won_awards: false,       books_written: 1,       created_at: Time.now)
+      @book1 = @author1.books.create!(      title: "Spiderwick",       best_seller: true,       pages: 120)
+      @book4 = @author1.books.create!(      title: "Wickspider",       best_seller: false,       pages: 300)
       @author2 = Author.create!(      name: " Peyton",       has_won_awards: true,       books_written: 2,       created_at: Time.now - 2.hours)
       @author3 = Author.create!(      name: " Gabe",       has_won_awards: false,       books_written: 3,       created_at: Time.now - 1.hours)
     end
 
     it("order by creation") do
       expect(Author.order_by_creation).to(eq([@author1, @author3, @author2]))
+    end
+
+    it("count_of_books") do
+      expect(@author1.book_count).to(eq(2))
     end
   end
 end
