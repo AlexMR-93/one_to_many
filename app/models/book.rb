b1802a110b3d900705a72007497c73d3949ffb31
Alex M <alexm0717@hotmail.com>
2022-10-14 18:38:52 -0700

    story fifteen complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
   validates_numericality_of :pages
   validates :best_seller,   inclusion: [true, false]
   belongs_to :author
+
+  def self.sort_by_true
+    self.where(    best_seller: :true)
+  end
 end
